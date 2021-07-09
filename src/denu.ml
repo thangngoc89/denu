9ae511fc9a8439987e77bff62371d640551282c4
Khoa Nguyen <hi@khoanguyen.me>
2021-07-09 23:01:56 +0700

    Fail when none/more than 1 entry file is provided

--- src/denu.ml
+++ src/denu.ml
@@ -3,11 +3,21 @@ let usage_msg = "Compile OCaml code: denu [--verbose] <entry_file>.ml"
 let speclist =
   [ ("--verbose", Arg.Set Main.verbose, "Output debug information") ]
 
-let entry_file = ref ""
+let input_files = ref []
 
-let anon_fun filename = entry_file := filename
+let anon_fun filename = input_files := filename :: !input_files
+
+let error_help_text = "\nRun denu --help for more information"
 
 let () =
   Arg.parse speclist anon_fun usage_msg;
-  Main.compile !entry_file
+  match !input_files with
+  | [] ->
+      prerr_endline
+        ("Exact one <entry_file>.ml file must be provided." ^ error_help_text);
+      exit 1
+  | [ entry_file ] -> Main.compile entry_file
+  | _x :: _tl ->
+      prerr_endline ("Too many <entry_file>.ml provided." ^ error_help_text);
+      exit 1
 
